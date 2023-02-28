6d3eafb76448830e48d871f944ff1646484bdf6f
Nihal Rajendra Shah <nihal@Nihals-MacBook-Pro.local>
2023-02-28 20:02:34 +0530

    Instance basic configuration

---
+++ instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "provisioner-instance" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  tags = {
+    "Name" = "Provisioner-Instance"
+  }
+}
